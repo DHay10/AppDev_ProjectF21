a381c199c0f9c790117a8f9e8da51cbb3293c566
sarahprimavera <sarahprimavera787@gmail.com>
2021-12-04 23:56:16 -0500

    did some documentation

--- ProjectV1/ProjectV1/DashboardView.cs
+++ ProjectV1/ProjectV1/DashboardView.cs
@@ -16,19 +16,19 @@ namespace ProjectV1
         {
             InitializeComponent();
         }
-
+        //add new student button
         private void addNewStudentButton_Click(object sender, EventArgs e)
         {
             NewStudentView newStudent = new NewStudentView();
             newStudent.ShowDialog();
         }
-
+        //search for student button
         private void searchStudentButton_Click(object sender, EventArgs e)
         {
             StudentFileView viewFile = new StudentFileView();
             viewFile.ShowDialog();
         }
-
+        //student database button
         private void studentdbB_Click(object sender, EventArgs e)
         {
             DatabaseView viewDB = new DatabaseView();
--- ProjectV1/ProjectV1/LoginView.cs
+++ ProjectV1/ProjectV1/LoginView.cs
@@ -15,15 +15,18 @@ namespace ProjectV1
         public LoginView()
         {
             InitializeComponent();
-            DBSystem.initData();
+            DBSystem.initData(); //initializing database system
         }
 
+        //login button method
         private void loginButton_Click(object sender, EventArgs e)
         {
+            //if user enters correct username and password, goes to dashboard
             if (usernameTB.Text == "user" && passwordTB.Text == "pass")
             {
-                this.Hide();
+                this.Hide();//hides login page
                 DashboardView mainMenu = new DashboardView();
+                //if close main menu, close whole system
                 mainMenu.FormClosed += (s, args) => this.Close();
                 mainMenu.Show();
             }
@@ -36,6 +39,7 @@ namespace ProjectV1
 
         private void OnKeyEnterDown(object sender, KeyEventArgs e)
         {
+            //if press enter either goes to net textbox or logs in
             if (e.KeyCode == Keys.Enter)
             {
                 if (usernameTB.Focused)
