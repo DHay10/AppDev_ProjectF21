df8969609fd9b26398f121914fb0cd5c4b7a6579
DHay10 <son-ta@hotmail.fr>
2021-12-04 19:25:34 -0500

    Implement DatabaseSystem using CSV

---
+++ ProjectV1/ProjectV1/DatabaseSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1
+{
+    class DatabaseSystem
+    {
+        protected static List<Student> students;
+        public static void initData()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"student.csv");
+            foreach(string line in lines)
+            {
+                string[] columns = line.Split(',');
+                students.Add(new Student(columns[1], columns[2], DateTime.Parse(columns[3]), columns[4], columns[5], columns[6], columns[7], columns[8], columns[9]));
+            }
+        }
+    }
+
+}
--- ProjectV1/ProjectV1/DatabaseView.Designer.cs
+++ ProjectV1/ProjectV1/DatabaseView.Designer.cs
@@ -34,8 +34,12 @@ namespace ProjectV1
             this.studentDBDataSet = new ProjectV1.StudentDBDataSet();
             this.studentTableAdapter = new ProjectV1.StudentDBDataSetTableAdapters.StudentTableAdapter();
             this.tableAdapterManager = new ProjectV1.StudentDBDataSetTableAdapters.TableAdapterManager();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // studentBindingSource
@@ -58,16 +62,39 @@ namespace ProjectV1
             this.tableAdapterManager.StudentTableAdapter = this.studentTableAdapter;
             this.tableAdapterManager.UpdateOrder = ProjectV1.StudentDBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(976, 512);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(730, 279);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView2.TabIndex = 1;
+            // 
             // DatabaseView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(864, 425);
+            this.ClientSize = new System.Drawing.Size(1000, 536);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "DatabaseView";
             this.Text = "DatabaseForm";
             this.Load += new System.EventHandler(this.DatabaseView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -77,5 +104,7 @@ namespace ProjectV1
         private System.Windows.Forms.BindingSource studentBindingSource;
         private StudentDBDataSetTableAdapters.StudentTableAdapter studentTableAdapter;
         private StudentDBDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
